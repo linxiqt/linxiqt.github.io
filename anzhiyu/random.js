7931fe8fdd8c27fa048db387005c4b83229e4c02
linxiqt <154610422+linxiqt@users.noreply.github.com>
2025-04-15 14:10:26 +0000

    Delete source/_posts/hello-world.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/15/give-cmd/","2025/04/15/hello-world/","2025/04/15/pojav/"];function toRandomPost(){
+var posts=["2025/04/15/give-cmd/","2025/04/15/pojav/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
