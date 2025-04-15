a93b22b2f4ae68a20c8d06c5de288148fd4c6fdc
linxiqt <154610422+linxiqt@users.noreply.github.com>
2025-04-15 09:41:24 +0000

    Save Post Draft source/_drafts/test.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/15/hello-world/"];function toRandomPost(){
+var posts=["2025/04/15/hello-world/","2025/04/15/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
