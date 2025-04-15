604f6751be8c4e5c5b9276332f8ea5b19e51e48d
linxiqt <154610422+linxiqt@users.noreply.github.com>
2025-04-15 11:37:42 +0000

    Delete Post Draft source/_drafts/你好.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/15/hello-world/","2025/04/15/test/"];function toRandomPost(){
+var posts=["2025/04/15/hello-world/","2025/04/15/test/","2025/04/15/你好/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
